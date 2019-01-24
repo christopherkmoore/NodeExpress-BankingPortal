f5445970bb06ce89660b08247c06483e83eeff39
christopherkmoore <christopherkmoore1@gmail.com>
2019-01-24 11:05:54 -0800

    adds section 2

--- src/app.js
+++ src/app.js
@@ -9,6 +9,19 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+const accountData = fs.readFileSync('src/json/accounts.json', { encoding: 'utf8' });
+const accounts = JSON.parse(accountData)
+
+const userData = fs.readFileSync('src/json/users.json', { encoding: 'utf8'} );
+const users = JSON.parse(userData)
+
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
+app.get('/savings', (request, response) => {
+    
+    response.render('account', { account: accounts.savings });
+});
+app.get('/checking', (request, response) => response.render('account', { account: accounts.checking }));
+app.get('/credit', (request, response) => response.render('account', { account: accounts.credit }));
+app.get('/profile', (request, response) => response.render('profile', { user: users[0] }));
 
 app.listen(3000, () => console.log('listening on port: 3000'))
